4342ab4a5e4878ccb047542bb1e2b2ce16f70d84
Math W <mathewsto51@gmail.com>
2021-06-02 08:03:34 -0300

    feat: index exports

---
+++ index.ts
@@ -0,0 +1,31 @@
+import BoxColorMode from './src/components/BoxColorMode';
+import BoxFile from './src/components/BoxFile';
+import { BoxMotion } from './src/components/BoxMotion';
+import BoxOverlay from './src/components/BoxOverlay';
+import BoxOverlaySpinner from './src/components/BoxOverlaySpinner';
+import BoxScrollMini from './src/components/BoxScrollMini';
+import Col from './src/components/Col';
+import ContainerScreen from './src/components/ContainerScreen';
+import SlideUpGroupHover from './src/components/GroupHover/SlideUpGroupHover';
+import SlideDownGroupHover from './src/components/GroupHover/SlideDownGroupHover';
+import SlideLeftGroupHover from './src/components/GroupHover/SlideLeftGroupHover';
+import SlideRightGroupHover from './src/components/GroupHover/SlideRightGroupHover';
+import Row from './src/components/Row';
+import TextMini from './src/components/TextMini';
+
+export const FSComponent = {
+  BoxColorMode,
+  BoxFile,
+  BoxMotion,
+  BoxOverlay,
+  BoxOverlaySpinner,
+  BoxScrollMini,
+  ContainerScreen,
+  SlideUpGroupHover,
+  SlideDownGroupHover,
+  SlideLeftGroupHover,
+  SlideRightGroupHover,
+  TextMini,
+  Col,
+  Row,
+}
